0666d8f2f6826db84d758b20eefb8e50a795d722
Mekaillzm <mekaillmurtaza@gmail.com>
2025-08-20 15:40:32 +0500

    Create questions.dart

---
+++ code/flutter/data/questions.dart
@@ -0,0 +1,38 @@
+import 'package:app5/models/quiz_questions.dart';
+
+const questions = [
+  QuizQuestion(
+    'What region are you in?',
+    [
+      'Lahore',
+      'Islamabad',
+      'Karachi',
+    ],
+  ),
+  QuizQuestion('What does the air quality feel like in your area?\nA higher score indicates better air quality', [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10'
+  ]),
+  QuizQuestion('What does the weather feel like in your area?\nA higher score indicates better weather conditions', [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10'
+  ]),
+
+
+];
